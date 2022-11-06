cd48809bf0ccd31079391f9eb7cb802eeab11b51
Whelagen Fixe2 <whelagen@gmail.com>
2022-11-06 17:39:50 +0100

    Travail sur la génération de document

--- lua/docgenerator/init.lua
+++ lua/docgenerator/init.lua
@@ -1,1 +1,35 @@
-print("appel")
+local M = {}
+
+
+local on_exit = function(job_id, code, _)
+    if code == 0 then
+        print("Conversion du document effectuée")
+    else
+        print("Erreur dans la conversion : " .. job_id:stderr_result()[1])
+    end
+end
+
+-- Capable de créer un fichier odt à partir d'un md
+M.genseance = function()
+    local nom = vim.fn.expand("%:p:r")
+    local cwd = vim.fn.getcwd()
+    vim.cmd [[:cd %:p:h]]
+    os.execute("cd")
+    local Job = require'plenary.job'
+    Job:new({
+        command = "pandoc",
+        args = {
+        -- "--template", "style.xml",
+        "-o", nom..".odt",
+vim.fn.expand("%:p")
+
+        },
+        on_exit = on_exit,
+    }):start()
+    vim.cmd(":cd ".. cwd)
+
+
+end
+
+
+return M
